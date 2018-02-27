95fa2e7b101363e3c6805229c62c911cc6ba3df4
hopes91 <nadezhdafalaleeva@gmail.com>
2018-02-27 11:24:19 +0300

    make scroll to the anchors slower

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
 	$('.recent-projects').fadeIn(2000);
+
+	const scrollToAnchor = (aid) => {
+		const aTag = $('#' + aid);
+		$('html, body').animate({scrollTop: aTag.offset().top}, 1500);
+	}
+
+	$('.projects__link').click(function() {
+		scrollToAnchor('projects');
+	});
+
+	$('.about-me__link').click(function() {
+		scrollToAnchor('about-me');
+	});
 });
