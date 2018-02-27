84dc45fcec8e132fded5cc9c4e7930c7d01bc3b1
hopes91 <nadezhdafalaleeva@gmail.com>
2018-02-27 11:53:53 +0300

    make current page highlighted

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 $(document).ready(function() {
 	$('.recent-projects').fadeIn(2000);
 
+	const currentUrl = window.location.href;
+	const activePage = currentUrl;
+
+	$('nav a').each(function() {
+		const linkPage = this.href;
+
+		if(activePage == linkPage) {
+			$(this).closest('a').addClass('active');
+		}
+	});
+
 	const scrollToAnchor = (aid) => {
 		const aTag = $('#' + aid);
 		$('html, body').animate({scrollTop: aTag.offset().top}, 1500);
