772a2ac4ff52d36344bdf872f379f18fb04eab40
hopes91 <nadezhdafalaleeva@gmail.com>
2018-03-01 21:39:55 +0300

    start working on parallax effect

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 $(document).ready(function() {
-	// Show recent projects in header
-	$('.recent-projects').fadeIn(2000);
+	// 
+	var currentX = '';
+	var currentY = '';
+	var movementConstant = .015;
+
+	$('.recent-projects').mousemove(function(e) {
+		if(currentX == '') currentX = e.pageX;
+		var xdiff = e.pageX - currentX;
+		currentX = e.pageX;
+
+		if(currentY == '') currentY = e.pageY;
+		var ydiff = e.pageY - currentY;
+		currentY = e.pageY;
+
+		$('.recent-projects').each(function(i, el) {
+			var movement = (i + 1) * (xdiff * movementConstant);
+			var movementy = (i + 1) * (ydiff * movementConstant);
+			var newX = $(el).position().left + movement;
+			var newY = $(el).position().top + movementy;
+			$(el).css('left', newX + 'px');
+			$(el).css('top', newY + 'px');
+		});
+	});
 
 	// Show the current page at nav
 	const currentUrl = window.location.href;
