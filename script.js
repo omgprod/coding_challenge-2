80ff5e21b8f044508c8fdfb5257c22bea67f3ae2
hopes91 <nadezhdafalaleeva@gmail.com>
2018-03-01 16:52:05 +0300

    trying to show only medium articles

--- script.js
+++ script.js
@@ -57,20 +57,26 @@ $(document).ready(function() {
 				console.log(response);
 				$.each(response.items, function(k, item) {
 					// $('<div></div>').appendTo('.medium-articles').addClass('article');
+					var categories = item.categories;
+					if(categories.length < 2) {
+						$('response.items').remove();
+						// return false;
+					} else {
+						$('<h3 class="medium-title">' + item.title + '</h3>').appendTo('.medium-articles');
+						$('<p class="medium-author">By ' + item.author + '</p>').appendTo('.medium-articles');
+						var yourString = item.description.replace(/<img[^>]*>/g,""); // Replace with your string
+						var maxLength = 120; // Maximum number of characters to extract
+						var trimmedString = yourString.substr(0, maxLength); // Trim the string to the maximum length
+						trimmedString = trimmedString.substr(0, Math.min(trimmedString.length, trimmedString.lastIndexOf(' '))); // Re-trim if we are in the middle of a word
+						$('<p class="medium-string">' + trimmedString + '...</p>').appendTo('.medium-articles');
+						$('<p class="medium-date">' + item.pubDate + '</p>').appendTo('.medium-articles');
+					}
 					// var tagIndex = item.description.indexOf('<img'); // Find where the img tag starts
 					// var srcIndex = item.description.substring(tagIndex).indexOf('src=') + tagIndex; // Find where the src attribute starts
 					// var srcStart = srcIndex + 5; // Find where the actual image URL starts; 5 for the length of 'src="'
 					// var srcEnd = item.description.substring(srcStart).indexOf('"') + srcStart; // Find where the URL ends
 					// var src = item.description.substring(srcStart, srcEnd); // Extract just the URL
 					// $('<div class="medium-pic"><img src="' + src + '" width="360px" height="240px"></div>').appendTo('.medium-article');
-					$('<h4>' + item.title + '</h4>').appendTo('.medium-articles');
-					$('<p>By ' + item.author + '</p>').appendTo('.medium-articles');
-					var yourString = item.description.replace(/<img[^>]*>/g,""); // Replace with your string
-					var maxLength = 200; // Maximum number of characters to extract
-					var trimmedString = yourString.substr(0, maxLength); // Trim the string to the maximum length
-					trimmedString = trimmedString.substr(0, Math.min(trimmedString.length, trimmedString.lastIndexOf(' '))); // Re-trim if we are in the middle of a word
-					$('<p>' + trimmedString + '...</p>').appendTo('.medium-articles');
-					$('<p class="pub-date">' + item.pubDate + '</p>').appendTo('.medium-articles');
 				});
 			}
 		});
